c2adfe249b0e3ffacc9c86999754dbeece9c70bb
Qi Wenmin <qiwenmin@gmail.com>
2022-03-30 18:50:08 +0800

    Generate new sheet uuid while creating project from the template

--- create-project.sh
+++ create-project.sh
@@ -7,9 +7,16 @@ fi
 
 projname=$1
 
-echo "Creating $projname project..."
+echo "Creating ${projname} project..."
 
-cp -r template $projname
-mv $projname/kicad/projectname.kicad_pro $projname/kicad/$projname.kicad_pro
-mv $projname/kicad/projectname.kicad_sch $projname/kicad/$projname.kicad_sch
-echo "# $projname" > $projname/README.md
+mkdir -p ${projname}/kicad
+
+cp template/kicad/fp-lib-table ${projname}/kicad/fp-lib-table
+cp template/kicad/sym-lib-table ${projname}/kicad/sym-lib-table
+
+sheet_uuid=$(uuidgen | tr '[:upper:]' '[:lower:]')
+
+sed "s/e63e39d7-6ac0-4ffd-8aa3-1841a4541b55/${sheet_uuid}/g" < template/kicad/projectname.kicad_pro > ${projname}/kicad/${projname}.kicad_pro
+sed "s/e63e39d7-6ac0-4ffd-8aa3-1841a4541b55/${sheet_uuid}/g" < template/kicad/projectname.kicad_sch > ${projname}/kicad/${projname}.kicad_sch
+
+echo "# ${projname}" > ${projname}/README.md
